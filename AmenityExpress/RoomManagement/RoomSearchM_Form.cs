7185159aa72533e97b0895e4c440dd3f88a39a35
Choi9033 <134060811+Choi9033@users.noreply.github.com>
2024-05-26 18:14:27 +0900

    test
    DB연동제외 등록수정삭제 기능 구현완료

--- AmenityExpress/RoomManagement/RoomSearchM_Form.cs
+++ AmenityExpress/RoomManagement/RoomSearchM_Form.cs
@@ -1,4 +1,5 @@
 ﻿using AmenityExpress;
+using AmenityExpress.RoomManagement;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -27,7 +28,7 @@ namespace AmenityExpress
             listView1.View = View.Details;
             listView1.FullRowSelect = true;
 
-            addButton.Click += new EventHandler(addButton_Click);
+            
 
         }
 
@@ -75,6 +76,54 @@ namespace AmenityExpress
 
                     listView1.Items.Add(item);
                 }
+            }
+        }
+
+        private void updateBtn_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedItem = listView1.SelectedItems[0];
+                Room room = new Room(
+                    int.Parse(selectedItem.SubItems[1].Text),
+                    selectedItem.SubItems[0].Text,
+                    int.Parse(selectedItem.SubItems[3].Text),
+                    int.Parse(selectedItem.SubItems[2].Text),
+                    selectedItem.SubItems[4].Text
+                );
+
+                using (DialogForm2 dialog = new DialogForm2(room))
+                {
+                    if (dialog.ShowDialog() == DialogResult.OK)
+                    {
+                        Room updatedRoom = dialog.RoomData;
+
+                        selectedItem.SubItems[0].Text = updatedRoom.Name;
+                        selectedItem.SubItems[1].Text = updatedRoom.Num.ToString();
+                        selectedItem.SubItems[2].Text = updatedRoom.Price.ToString();
+                        selectedItem.SubItems[3].Text = updatedRoom.MaxP.ToString();
+                        selectedItem.SubItems[4].Text = updatedRoom.Notice;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("수정할 항목을 선택하세요.");
+            }
+        }
+
+        private void delBtn_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedItem = listView1.SelectedItems[0];
+
+                listView1.Items.Remove(selectedItem);
+
+            }
+            else
+            {
+                MessageBox.Show("삭제할 항목을 선택하세요.");
             }
         }
     }
